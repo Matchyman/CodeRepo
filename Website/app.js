4d7e470303c89f95439c9559a4ab84f15b891648
Matchyman <jmatcham31@googlemail.com>
2021-05-08 17:41:39 +0100

    Stuff and things

--- Website/app.js
+++ Website/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var hostname = '127.0.0.1';
+var hostname = 'localhost';
 var port = 3000;
 var app = express();
 
@@ -26,5 +26,5 @@ app.get('/contact', (req, res) => {
 
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
---
+++ Website/public/scripts/scripts.js
@@ -0,0 +1,13 @@
+//Functions for buttons changing pages
+
+function redirectHome() {
+    location.replace("/")
+}
+
+function redirectAbout() {
+    location.replace("/about");
+}
+
+function redirectContact() {
+    location.replace("/contact")
+}
